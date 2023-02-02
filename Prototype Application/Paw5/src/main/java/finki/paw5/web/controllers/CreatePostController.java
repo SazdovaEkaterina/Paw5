cfc3e817ff0ec105d0cbe244f66850c38626680a
Filip Chorbeski <86695898+FilipChorbeski@users.noreply.github.com>
2023-02-02 10:54:25 +0100

    Created html and controller for post

---
+++ Prototype Application/Paw5/src/main/java/finki/paw5/web/controllers/CreatePostController.java
@@ -0,0 +1,10 @@
+package finki.paw5.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/CreatePost")
+public class CreatePostController {
+
+}
