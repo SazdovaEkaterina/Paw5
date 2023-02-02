4885da38765764f3ee56235d6b6ceed7c4529c23
SazdovaEkaterina <sazdovaekaterina@gmail.com>
2023-02-02 21:01:19 +0100

    create blank repo service templates controllers

---
+++ Prototype Application/Paw5/src/main/java/finki/paw5/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package finki.paw5.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository {
+}
---
+++ Prototype Application/Paw5/src/main/java/finki/paw5/web/controllers/PetController.java
@@ -0,0 +1,15 @@
+package finki.paw5.web.controllers;
+
+import finki.paw5.service.PetService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PetController {
+
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+}
