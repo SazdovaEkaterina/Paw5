a2060cbb8d0eb743b7af20503f51565aad7426cf
Filip Chorbeski <86695898+FilipChorbeski@users.noreply.github.com>
2023-02-05 21:13:30 +0100

    remove duplicate import
    Co-Authored-By: SazdovaEkaterina <74919977+SazdovaEkaterina@users.noreply.github.com>

--- Prototype Application/Paw5/src/main/java/finki/paw5/repository/PetRepository.java
+++ Prototype Application/Paw5/src/main/java/finki/paw5/repository/PetRepository.java
@@ -1,7 +1,5 @@
 package finki.paw5.repository;
 
-import org.springframework.stereotype.Repository;
-
 import finki.paw5.model.entities.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
