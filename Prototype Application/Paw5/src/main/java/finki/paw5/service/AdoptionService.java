e65d64516210592987a4d77608b4007c2eaebf8a
SazdovaEkaterina <sazdovaekaterina@gmail.com>
2023-06-23 18:36:57 +0200

    add adoption service logic

--- Prototype Application/Paw5/src/main/java/finki/paw5/service/AdoptionService.java
+++ Prototype Application/Paw5/src/main/java/finki/paw5/service/AdoptionService.java
@@ -2,7 +2,17 @@ package finki.paw5.service;
 
 import finki.paw5.model.entities.Adoption;
 
+import java.util.List;
+
 public interface AdoptionService {
 
     void save(Adoption adoption);
+
+    Adoption findById(Integer id);
+
+    List<Adoption> findAll();
+
+    List<Adoption> findAllThatNeedApproval();
+
+    Adoption markApproved(Integer id);
 }
--- Prototype Application/Paw5/src/main/java/finki/paw5/service/implementation/AdoptionServiceImplementation.java
+++ Prototype Application/Paw5/src/main/java/finki/paw5/service/implementation/AdoptionServiceImplementation.java
@@ -5,6 +5,10 @@ import finki.paw5.repository.AdoptionRepository;
 import finki.paw5.service.AdoptionService;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AdoptionServiceImplementation implements AdoptionService {
 
@@ -17,5 +21,30 @@ public class AdoptionServiceImplementation implements AdoptionService {
     @Override
     public void save(Adoption adoption) {
         this.adoptionRepository.save(adoption);
+    }
+
+    @Override
+    public Adoption findById(Integer id) {
+        return this.adoptionRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Adoption> findAll() {
+        return this.adoptionRepository.findAll();
+    }
+
+    @Override
+    public List<Adoption> findAllThatNeedApproval() {
+        return this.adoptionRepository.findAll().stream()
+                .filter(a -> !a.getApproved())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Adoption markApproved(Integer id) {
+        Adoption adoption = this.findById(id);
+        adoption.setApproved(true);
+        this.adoptionRepository.delete(this.findById(id));
+        return this.adoptionRepository.save(adoption);
     }
 }
